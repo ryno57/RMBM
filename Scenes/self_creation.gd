8f125f10a6a72d1788fce1e6d5984c699c348fc9
Lothevy <45122731+ryno57@users.noreply.github.com>
2025-11-12 23:15:28 +0100

    DEV B3.1.0
    currently working on Main scenes (actual gameplay scene)
    made the toolbar with radiobutton (icon), currently there only [company] and [buildings] that are clickable.
    I also spedup the splashscreen

--- Scenes/self_creation.gd
+++ Scenes/self_creation.gd
@@ -1,7 +1,8 @@
 extends Control
 
-
-
-
 func _on_back_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
--- scripts/splash.gd
+++ scripts/splash.gd
@@ -4,4 +4,4 @@ func _ready():
 	connect("finished", Callable(self, "_on_video_finished"))
 
 func _on_video_finished():
-	get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")  # Remplace par le chemin de ta scène principale
+	get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")
